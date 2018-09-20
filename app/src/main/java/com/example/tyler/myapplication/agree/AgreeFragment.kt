5c1e10d570af4417bab2fd2dc7b3d23a6d14cff3
tpfaff <tylerpfaff@gmail.com>
2018-09-20 03:05:41 -0700

    Show error state

--- app/src/main/java/com/example/tyler/myapplication/agree/AgreeFragment.kt
+++ app/src/main/java/com/example/tyler/myapplication/agree/AgreeFragment.kt
@@ -62,6 +62,7 @@ class AgreeFragment : Fragment() {
                 },
                         { error ->
                             Log.e(TAG, "uiStateError $error")
+                            showErrorState()
                         }))
     }
 
